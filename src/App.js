046293dc7ad79abb30dc63abdb2249406f74b75d
Antonio <berser.ar@gmail.com>
2016-06-21 00:02:45 -0500

    Added icons and fonts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class App extends Component {
   render() {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
+        <Icon.ToolbarAndroid
+          title='Retos'
+          elevation={3}
+          titleColor='white'
+          style={{ backgroundColor: '#2eb2ff', height: 56 }}
+        />
 
+        <View style={{ flex: 1 }}>
+          <View style={styles.game}>
+            <Text>Hello</Text>
+          </View>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  game: {
+    backgroundColor: 'red',
+    height: 60
+  }
+})
